210cdc0a173ecf3fc6c942fe63cafbd81fc650d5
Eva813 <as45986@gmail.com>
2021-04-29 13:19:42 +0800

    const remove

--- 01-JS-ES6/exercise/js/Tools.js
+++ 01-JS-ES6/exercise/js/Tools.js
@@ -5,6 +5,10 @@ const Add = (a, b) => {
 //純粹一個export的使用，就是選擇性要不要丟出去
 export const Name = 'Mike';
 export const age = 28;
+export const remove = (a, b) => {
+  return a - b;
+}
+
 
 //使用export default，當要載入module時，這個default就會直接被推出去
 export default Add;
